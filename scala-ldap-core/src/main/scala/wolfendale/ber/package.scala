e835910c5b6d929d0da880296b8f415f83f5e0f3
Michael Wolfendale <4563722+wolfendale@users.noreply.github.com>
2020-10-07 20:07:17 +0100

    Add BER TLV encoding

--- scala-ldap-core/src/main/scala/wolfendale/ber/package.scala
+++ scala-ldap-core/src/main/scala/wolfendale/ber/package.scala
@@ -1,6 +1,6 @@
 package wolfendale
 
-import scodec.{Attempt, Codec, DecodeResult, SizeBound}
+import scodec._
 import scodec.bits._
 import scodec.codecs._
 
@@ -8,7 +8,7 @@ import scala.annotation.tailrec
 
 package object ber {
 
-  def berInt: Codec[Int] = new Codec[Int] {
+  val berInt: Codec[Int] = new Codec[Int] {
 
     override def sizeBound: SizeBound = SizeBound.bounded(8, 8 * 5)
 
@@ -40,4 +40,44 @@ package object ber {
       } else Attempt.successful(buffer)
     }
   }
+
+  private def sized[A](codec: Codec[A]): Codec[A] = new Codec[A] {
+
+    private def efficientSize(long: Long): Int = {
+      @tailrec
+      def inner(long: Long, size: Int): Int = {
+        val next = long >> 8
+        if (next > 0) inner(next, size + 1) else size
+      }
+      inner(long, 1)
+    }
+
+    private val lengthCodec: Codec[Long] = {
+      bool.consume {
+        case false => ulong(7)
+        case true  => ulong(7).consume {
+          case 0                 => fail[Long](Err("Content of indefinite length is not supported"))
+          case size if size <= 8 => ulong(size.toInt * 8)
+          case 127               => fail[Long](Err("Initial length value of 0xFF is reserved"))
+          case _                 => fail[Long](Err("Content lengths that are greater than (2 ^ 64) bytes are not supported"))
+        } (efficientSize)
+      } (java.lang.Long.compareUnsigned(_, 127L) > 0)
+    }
+
+    private val decoder: Decoder[A] =
+      lengthCodec.flatMap(fixedSizeBytes(_, codec))
+
+    override def sizeBound: SizeBound = SizeBound.atLeast(8)
+
+    override def encode(value: A): Attempt[BitVector] = for {
+      content <- codec.encode(value)
+      size    <- lengthCodec.encode(content.size / 8)
+    } yield size ++ content
+
+    override def decode(bits: BitVector): Attempt[DecodeResult[A]] =
+      decoder.decode(bits)
+  }
+
+  def ber[A](identifier: Identifier, codec: Codec[A]): Codec[A] =
+    Identifier.codec.unit(identifier) ~> sized(codec)
 }
--- scala-ldap-core/src/test/scala/wolfendale/ber/BerTest.scala
+++ scala-ldap-core/src/test/scala/wolfendale/ber/BerTest.scala
@@ -3,23 +3,66 @@ package wolfendale.ber
 import org.scalatest._
 import freespec._
 import matchers.must._
-import scodec.bits._
 import wolfendale.EncodingAssertions
+import scodec._
+import bits._
 
 class BerTest extends AnyFreeSpec with Matchers with EncodingAssertions {
 
-  "berInt" - {
+  "BerTest" - {
 
-    "0" - mustEncode(berInt, 0, bin"00000000")
-    "1" - mustEncode(berInt, 1, bin"00000001")
-    "127" - mustEncode(berInt, 127, bin"01111111")
-    "128" - mustEncode(berInt, 128, bin"10000001 00000000")
-    "16383" - mustEncode(berInt, 16383, bin"11111111 01111111")
-    "16384" - mustEncode(berInt, 16384, bin"10000001 10000000 00000000")
-    "Int.MaxValue" - mustEncode(berInt, Int.MaxValue, bin"10000111 11111111 11111111 11111111 01111111")
+    "ber" - {
 
-    "Binary strings which would decode to a value greater than Int.MaxValue must fail instead" ignore {
-      // TODO
+      "empty content" - {
+        mustEncode(
+          ber[BitVector](Identifier(TagClass.Universal, TagType.Null), codecs.bits(0)),
+          BitVector.empty,
+          bin"00000101 00000000"
+        )
+      }
+
+      "short content" - {
+        mustEncode(
+          ber[ByteVector](Identifier(TagClass.Universal, TagType.OctetString(ConstructionType.Primitive)), codecs.bytes(1)),
+          ByteVector.high(1),
+          bin"00000100 00000001 11111111"
+        )
+      }
+
+      "short content (max)" - {
+        mustEncode(
+          ber[ByteVector](Identifier(TagClass.Universal, TagType.OctetString(ConstructionType.Primitive)), codecs.bytes(127)),
+          ByteVector.high(127),
+          (bin"00000100 01111111" ++ ByteVector.high(127).bits).compact
+        )
+      }
+
+      "long content" - {
+        mustEncode(
+          ber[ByteVector](Identifier(TagClass.Universal, TagType.OctetString(ConstructionType.Primitive)), codecs.bytes(128)),
+          ByteVector.high(128),
+          (bin"00000100 10000001 10000000" ++ ByteVector.high(128).bits).compact
+        )
+      }
+
+      "content of indefinite length is not supported" in {
+        val codec = ber(Identifier(TagClass.Universal, TagType.OctetString(ConstructionType.Primitive)), codecs.bytes(1))
+        val result = codec.decode(bin"00000100 10000000 11111111")
+        result.isFailure mustBe true
+      }
+
+      "content with a reserved length octet should be rejected" in {
+        val codec = ber(Identifier(TagClass.Universal, TagType.OctetString(ConstructionType.Primitive)), codecs.bytes(1))
+        val result = codec.decode(bin"00000100 11111111 11111111")
+        result.isFailure mustBe true
+      }
+
+      "content with a content length represented in more than 64 bits is unsupported" in {
+        val codec = ber(Identifier(TagClass.Universal, TagType.OctetString(ConstructionType.Primitive)), codecs.bytes(1))
+        val result = codec.decode(bin"00000100 10001001 11111111")
+        result.isFailure mustBe true
+        result.asInstanceOf[Attempt.Failure].cause.message mustEqual "Content lengths that are greater than (2 ^ 64) bytes are not supported"
+      }
     }
   }
 }
